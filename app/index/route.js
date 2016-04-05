91648a4835dbef59d9173735f32c9f8007cdca3e
Lauryn Davis and Molly Curtin <patternandshape@gmail.com>
2016-04-05 09:47:48 -0700

    add working list of senate and congress committees on index page

--- app/index/route.js
+++ app/index/route.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    var url = 'http://congress.api.sunlightfoundation.com/committees?subcommittee=false&apikey=209a3433698a40978a2a5fb6c519e27e';
+
+      return Ember.$.getJSON(url).then(function(responseJSON){
+        return responseJSON.results;
+      });
+  },
   actions: {
     zipLookup(params) {
       this.transitionTo('results', params.zip);
