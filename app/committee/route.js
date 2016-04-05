c08c059e7f90719d3626765c9842be83c9f2d648
Lauryn Davis and Molly Curtin <patternandshape@gmail.com>
2016-04-05 15:25:46 -0700

    add working dynamically routed committee pages which list each committees subcommittee

--- app/committee/route.js
+++ app/committee/route.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return ('committee', committee_id);
-  }
+  model: function(params) {
+    var committeeUrl = 'http://congress.api.sunlightfoundation.com/committees?committee_id=' + params.committee_id + '&subcommittee=false&apikey=209a3433698a40978a2a5fb6c519e27e';
+
+    var subCommitteeUrl =
+    'http://congress.api.sunlightfoundation.com/committees?subcommittee=true&parent_committee_id=' + params.committee_id + '&apikey=209a3433698a40978a2a5fb6c519e27e';
+
+    return Ember.RSVP.hash({
+      committees: Ember.$.getJSON(committeeUrl).then(function(responseJSON){
+          return responseJSON.results;
+        }),
+      subcommittees: Ember.$.getJSON(subCommitteeUrl).then(function(responseJSON){
+        return responseJSON.results;
+      })
+    });
+  },
 });
