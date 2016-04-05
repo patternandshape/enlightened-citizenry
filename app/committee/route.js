33ffda71b4139de68fc5abe1fb74270a5b7e3988
Lauryn Davis and Molly Curtin <patternandshape@gmail.com>
2016-04-05 12:12:37 -0700

    add api call to get bills to index route

--- app/committee/route.js
+++ app/committee/route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
-   return this.store.findRecord('committee', params.committee_id);
- }
+  model() {
+    return ('committee', committee_id);
+  }
 });
--- app/index/route.js
+++ app/index/route.js
@@ -4,13 +4,18 @@ export default Ember.Route.extend({
   model: function() {
     var committeeUrl = 'http://congress.api.sunlightfoundation.com/committees?subcommittee=false&apikey=209a3433698a40978a2a5fb6c519e27e';
 
+    var billUrl = 'http://congress.api.sunlightfoundation.com/bills?apikey=209a3433698a40978a2a5fb6c519e27e';
+
     return Ember.RSVP.hash({
       committees: Ember.$.getJSON(committeeUrl).then(function(responseJSON){
           return responseJSON.results;
-        })
+        }),
+      bills: Ember.$.getJSON(billUrl).then(function(responseJSON){
+        return responseJSON.results;
+      })
     });
-
   },
+
   actions: {
     zipLookup(params) {
       this.transitionTo('results', params.zip);
