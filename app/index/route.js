b8d3adec14faadb7a5929d1f45f84eff4bd6e897
Lauryn Davis and Molly Curtin <lryndavis@gmail.com>
2016-04-05 11:37:46 -0700

    alter committee model in index route to include api variable

--- app/index/route.js
+++ app/index/route.js
@@ -2,11 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var url = 'http://congress.api.sunlightfoundation.com/committees?subcommittee=false&apikey=209a3433698a40978a2a5fb6c519e27e';
+    var committeeUrl = 'http://congress.api.sunlightfoundation.com/committees?subcommittee=false&apikey=209a3433698a40978a2a5fb6c519e27e';
 
-      return Ember.$.getJSON(url).then(function(responseJSON){
-        return responseJSON.results;
-      });
+    return Ember.RSVP.hash({
+      committees: Ember.$.getJSON(committeeUrl).then(function(responseJSON){
+          return responseJSON.results;
+        })
+    });
+
   },
   actions: {
     zipLookup(params) {
